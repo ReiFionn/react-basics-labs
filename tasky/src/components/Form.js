2f4f08809238a798f5d013d8b572d0b48cf113ba
reifionn <20101977@mail.wit.ie>
2024-09-30 18:51:41 +0100

    Tasky Lab 2 - Exercises done?

--- tasky/src/components/Form.js
+++ tasky/src/components/Form.js
@@ -3,22 +3,36 @@ import React from "react";
 const AddTaskForm = (props) => {
   return (
     <div>
-        <form>
+        <form onSubmit={props.submit}>
             <label>
                 Task title:
-                <input type="text" name="title" required />
+                <br></br>
+                <input type="text" name="title" required onChange={(event) => props.change(event)} />
             </label>
             <br />
             <label>
                 Due date:
-                <input type="date" name="deadline" required />
+                <br></br>
+                <input type="date" name="deadline" required onChange={(event) => props.change(event)} />
             </label>
             <br />
             <label>
                 Details:
-                <input type="text" name="description" />
+                <br></br>
+                <input type="text" name="description" onChange={(event) => props.change(event)} />
             </label>
-            <input type="submit" value="Submit" />
+            <br />
+            <label>
+                Priorty:
+                <br></br>
+                <select name="priority" required onChange={(event) => props.change(event)}>
+                    <option disabled selected value> -- select an option -- </option>
+                    <option value="Low">Low</option>
+                    <option value="Medium">Medium</option>
+                    <option value="High">High</option>
+                </select>
+            </label>
+            <input type="submit"></input>
         </form>
     </div>
   )
--- tasky/src/components/Task.js
+++ tasky/src/components/Task.js
@@ -7,6 +7,7 @@ const Task = (props) => {
             <p className="title">{props.title}</p>
             <p>Due: {props.deadline}</p>
             <p className="description">{props.description}</p>
+            <p className="priority" style={{backgroundColor: props.priority === "Medium" ? 'orange' : props.priority === "High" ? 'red' : 'green'}}>{props.priority}</p>
             <button onClick={props.markDone} className='doneButton'>Done</button>
             <button className='deleteButton' onClick={props.deleteTask}>Delete</button>
         </div>
